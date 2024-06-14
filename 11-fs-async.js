e6f277638a63f1f4d4adf4e22b99718d8bd0f0ab
Kumar_Anshuman <kanshuman6484@gmail.com>
2024-06-14 13:36:19 +0530

    added fs-module

---
+++ 11-fs-async.js
@@ -0,0 +1,39 @@
+// Asynchronous method
+
+const { readFile, writeFile } = require('fs')
+
+// readFile(path,encoding type like 'utf8', callback fun)
+
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   console.log(result)
+// })
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  const first = result
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const second = result
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(result)
+      }
+    )
+  })
+})
