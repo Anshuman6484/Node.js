c3beb3c8281b4b849f534ab447d570d5f715b9dd
Kumar_Anshuman <kanshuman6484@gmail.com>
2024-06-14 20:56:32 +0530

    start of node tutorial

--- 1-intro.js
+++ 1-intro.js
@@ -3,4 +3,4 @@ const amount = 8
 if (amount < 10) console.log('Small amount')
 else console.log('Big amount')
 
-console.log(`Hey! I just created my first node app!!!`)
+console.log(`Hey! I just created my first node app!!`)
