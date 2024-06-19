16baff6a720dcc4a7470ed2fb3c5ba9f1b445d1d
Kumar_Anshuman <kanshuman6484@gmail.com>
2024-06-19 09:09:58 +0530

    added modules

---
+++ 1-node-tutorial/03-modules.js
@@ -0,0 +1,23 @@
+// CommonJs, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+// const amit = 'Amit'
+// const sumit = 'Sumit'
+
+// const sayHi = (name) => {
+//   console.log(`Hey! ${name}`)
+// }
+
+const names = require('./4-modules-I(names)') // Use require to acess the exported module
+const sayHi = require('./5-modules-II(utils)')
+const data = require('./6-alt-method')
+
+require('./7-mind-grenade') // It works since we envoked the addValues fun in the mind-grenade file
+
+// console.log(names)
+
+// sayHi('Anshuman')
+// sayHi(names.amit)
+// sayHi(names.sumit)
+
+// console.log(data)
