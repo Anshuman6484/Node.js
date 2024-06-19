25f08fbe96e2d35f784b771649713525ad79c3f4
Kumar_Anshuman <kanshuman6484@gmail.com>
2024-06-19 09:13:34 +0530

    removed unneccesary files

--- 1-node-tutorial/5-modules-II(utils).js
+++
@@ -1,7 +0,0 @@
-const sayHi = (name) => {
-  console.log(`Hey! ${name}`)
-}
-
-// Exporting a fun
-
-module.exports = sayHi // Since only one thing is being exported, no need to export in form of object
