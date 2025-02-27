a0e72393d015e2156ad9dfd5624ff803ccaca146
Kumar_Anshuman <kanshuman6484@gmail.com>
2025-02-27 17:40:37 +0530

    express

--- 02-express-tutorial/04-express-app.js
+++ 02-express-tutorial/04-express-app.js
@@ -6,9 +6,12 @@ const app = express()
 // setup static and middleware
 app.use(express.static('./navbar-app'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//   // Alternate methods for sending index.html
+//   // add to static assests
+//   // SSR
+// })
 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
